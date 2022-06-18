34754033a8f4f870f123f6cfe93949de853af97f
soso01 <soso01dev@gmail.com>
2022-06-18 13:06:15 +0900

    <feat> loginview 추가

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,7 +1,11 @@
+import { PATH } from "@/common/constants";
+import Link from "next/link";
+
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
+      <Link href={PATH.LOGIN}>로그인</Link>
     </div>
   );
 }
---
+++ apps/web/src/pages/login.tsx
@@ -0,0 +1,7 @@
+import { LoginView } from "@/domains/auth/views";
+
+const LoginPage = () => {
+  return <LoginView />;
+};
+
+export default LoginPage;
