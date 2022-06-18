8b786e902fef918801ce4951fb68efdfef8fbfba
soso01 <soso01dev@gmail.com>
2022-06-18 20:01:18 +0900

    <feat> firebase init

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { useEffect } from "react";
 
 import { theme } from "@/styles";
+import { initializeFirebase } from "@/common/settings/firebase";
 
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    initializeFirebase();
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
